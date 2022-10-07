4b8e37a291f13b46c66f01fd0e1f9d3dc0041c27
Jeyna Kim <jeyna.kimm@gmail.com>
2022-10-06 20:14:10 -0400

    complete 9-palindrom-number.js

---
+++ Easy/9-palindrome-number.js
@@ -0,0 +1,15 @@
+/* Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function(x) {
+  const reversedNumber = x.toString().split('').reverse().join('');
+  return (x == reversedNumber ? true : false);
+};
+
+
