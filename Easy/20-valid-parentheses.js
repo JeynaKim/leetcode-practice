7f3052af4286140f8d8df6825bd1c6ede5a88aba
Jeyna Kim <jeyna.kimm@gmail.com>
2022-10-12 17:35:16 -0400

    complete 20-valid-parentheses.js

---
+++ Easy/20-valid-parentheses.js
@@ -0,0 +1,30 @@
+/* 
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const isValid = (s) => {
+  let result = true;
+  let y = [];
+  let a = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (let ele of s) {
+    if (["(", "[", "{"].includes(ele)) {
+      y.push(ele);
+    } else if (a[ele] === y[y.length - 1]) {
+      y.pop();
+    } else {
+      return false;
+    }
+  }
+  return result;
+};
