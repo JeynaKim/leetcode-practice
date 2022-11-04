0582c80e08ec8256a25f5f8336e4218232e7b7aa
Jeyna Kim <jeyna.kimm@gmail.com>
2022-11-04 17:07:39 -0400

    add 67 questions

---
+++ Easy/67-add-binary.js
@@ -0,0 +1,13 @@
+// Given two binary strings a and b, return their sum as a binary string //
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    
+ };
+
+
+
