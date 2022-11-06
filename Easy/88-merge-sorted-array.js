aaa2b9cf0f9d4ce448c7fe94eb8a3832626d5caa
Jeyna Kim <jeyna.kimm@gmail.com>
2022-11-06 11:31:48 -0500

    add 88 question

---
+++ Easy/88-merge-sorted-array.js
@@ -0,0 +1,16 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+var merge = function(nums1, m, nums2, n) {
+    
+};
